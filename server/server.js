060fc37a604f783b9d494fc2d9b4eaeffe11cec5
Cihad Cengiz <cihad@extramus.eu>
2022-07-19 12:56:20 +0300

    static file serving fix#2

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ require('./api')(app)
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.resolve(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "client", "build")));
    // Handle React routing, return all requests to React app  
    app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
