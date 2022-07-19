6b18043b606db595ec9e3905d579001153e9befe
Cihad Cengiz <cihad@extramus.eu>
2022-07-19 12:46:34 +0300

    another try

--- server/server.js
+++ server/server.js
@@ -21,9 +21,11 @@ require('./api')(app)
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
   app.use(express.static(path.resolve(__dirname, "/client/build")));
-   // Handle React routing, return all requests to React app  app.get("*", function (req, res) {
+   // Handle React routing, return all requests to React app  
+   app.get("*", function (req, res) {
     res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
-  }
+  });
+} 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
